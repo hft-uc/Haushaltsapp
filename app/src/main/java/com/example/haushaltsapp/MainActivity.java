bd490035e1e964af08ddc6d95e00f615e7c6c7a3
Dennis Weggenmann <d.weggenmann@web.de>
2021-03-31 09:11:19 +0200

    added Firebase registration

--- app/src/main/java/com/example/haushaltsapp/MainActivity.java
+++ app/src/main/java/com/example/haushaltsapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.haushaltsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
