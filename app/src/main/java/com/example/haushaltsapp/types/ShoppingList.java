2323a85311611b99906503ab506f8a970edea72c
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-03-31 08:56:30 +0200

    Add dto classes

---
+++ app/src/main/java/com/example/haushaltsapp/types/ShoppingList.java
@@ -0,0 +1,50 @@
+package com.example.haushaltsapp.types;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+class ShoppingList {
+
+    @NonNull
+    private List<ShoppingListEntry> entries;
+
+    @NonNull
+    private User owner;
+
+    @NonNull
+    private List<User> members;
+
+    public ShoppingList(@NonNull List<ShoppingListEntry> entries, @NonNull User owner, @NonNull List<User> members) {
+        this.entries = entries;
+        this.owner = owner;
+        this.members = members;
+    }
+
+    @NonNull
+    public List<ShoppingListEntry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(@NonNull List<ShoppingListEntry> entries) {
+        this.entries = entries;
+    }
+
+    @NonNull
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(@NonNull User owner) {
+        this.owner = owner;
+    }
+
+    @NonNull
+    public List<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(@NonNull List<User> members) {
+        this.members = members;
+    }
+}
