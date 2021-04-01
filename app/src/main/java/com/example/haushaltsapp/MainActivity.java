3d66e9e23799c9e80a4953908f641c9cb3f4e2ca
Dennis Weggenmann <d.weggenmann@web.de>
2021-04-01 11:59:17 +0200

    added login and registration

--- app/src/main/java/com/example/haushaltsapp/MainActivity.java
+++ app/src/main/java/com/example/haushaltsapp/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.haushaltsapp;
 
 
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button showEmailbtn;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showEmailbtn = findViewById(R.id.button1);
+        showEmailbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String us = user.getEmail();
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, us, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
