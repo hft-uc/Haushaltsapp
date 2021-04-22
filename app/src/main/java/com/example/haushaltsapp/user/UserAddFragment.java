e4ead5314eb8127f2b4fb8b3b23caf0d01614875
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-04-22 19:38:12 +0200

    Start UserAddFragment
    users can be added to here

---
+++ app/src/main/java/com/example/haushaltsapp/user/UserAddFragment.java
@@ -0,0 +1,23 @@
+package com.example.haushaltsapp.user;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.haushaltsapp.R;
+
+public class UserAddFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_add_list, container, false);
+
+        return view;
+    }
+}
