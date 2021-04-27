5b00a0ba7d1680440ea99b81f570c39566ec56fa
MarieNabhan <82nama1bif@hft-stuttgart.de>
2021-04-27 20:29:09 +0200

    modified id

--- app/src/main/java/com/example/haushaltsapp/types/SupplyEntry.kt
+++ app/src/main/java/com/example/haushaltsapp/types/SupplyEntry.kt
@@ -3,11 +3,14 @@ package com.example.haushaltsapp.types
 import java.time.ZonedDateTime
 
 data class SupplyEntry(
-        val id: Int,
+        //val id: Int,
+        var id: String,
         var name: String,
         var amount: String,
         var expiry: ZonedDateTime?
 ) {
     @Suppress("unused")
-    constructor() : this(0, "", "", null)
+    constructor() : this("", "", "", null)
+
+
 }
